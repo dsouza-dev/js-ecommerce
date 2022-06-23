5e80af527b5098fd960faa98f51a212f54d9fd16
Daniel Souza <workdndsza@gmail.com>
2022-06-22 23:32:11 -0300

    adicionando sessão no express com a express-session

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 require('./utils/db.config')
 
@@ -7,10 +8,20 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
+// app.set('trust proxy', 1)
+app.use(session({
+  secret: 'fcd6a5c874758d532f8b737b34e95fc9945763d4',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 
 app.use('/', authRoutes)
 
 app.get('/', (req, res) => {
+  req.session.views = (req.session.views || 0) + 1
+  console.log(`Você visitou ${req.session.views} vezes`)
+
   return res.render('index')
 })
 
